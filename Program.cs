5420954eb9f8683644875392d15af0568048489b
YaxshiboyZiyatov <ziyatovyaxshiboy114@gmail.com>
2021-11-26 21:02:28 +0500

    commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Triangles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 4;
+            Console.Write(checkTriangles(n));
+        }
+        public static int checkTriangles(int n)
+        {
+
+            return (n * (n + 2) * (2 * n + 1) / 8);
+        }
+    }
+}
